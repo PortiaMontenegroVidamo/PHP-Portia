df24bc750c10a9b7b4f7f39cde7fd37d075d0b76
PortiaMontenegroVidamo <50134994+PortiaMontenegroVidamo@users.noreply.github.com>
2019-05-16 13:14:14 +0800

    Add files via upload

---
+++ php-portia/delete.php
@@ -0,0 +1,58 @@
+
+
+<?php require "db.php"; ?>
+<?php include "functions.php"; ?>
+
+<?php
+if(isset($_POST['submit'])){
+	
+	deleteTable();
+
+	 if(!$result){
+	 	die("QUERY FAILED" . mysqli_error());
+	 }
+
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>DELETE</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container border border-primary col-lg-6 mx-auto rounded-lg">
+		<h1>DELETE</h1>
+		<div>
+			<form method="post" action="delete.php">
+				<div class="form-group">
+					<label>Username:</label>
+					<input type="text" name="username" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Password:</label>
+					<input type="password" name="password" class="form-control">
+					
+				</div>
+				<select name="id" id="">
+					<?php
+							showData();
+					?>
+				</select>
+
+				<br>
+				<input type="submit" name="submit" value="DELETE" class="btn btn-primary">
+
+					
+
+			</form>
+		</div>
+	</div>			
+
+</body>
+</html>
